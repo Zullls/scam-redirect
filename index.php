342a40878f2ad1ef77252a0a351e7e64c1b288c5
Zullls <76924369+Zullls@users.noreply.github.com>
2021-09-06 01:36:46 +0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,173 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- META -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LINK -->
+    <link rel="stylesheet" type="text/css" href="src/css/style.css">
+    <title>Chat</title>
+    <!-- FACE -->
+</head>
+<body>
+<div class="img-center">
+    <img src="src/img/awp.png" alt = "awp">
+        </div>
+            <div class="thickfont">
+                <p>
+                    https://steamcommunity.com/steamhypeb...
+                    </p>
+                        </div>
+                            <div class="font">
+                                    <p>
+                                Add our bot to your friends on steam and get your first skin
+                            </p>
+                        </div>
+                    <button onclick="window.location.href='https://steamcommunity.com'"class="btn1">ADD FRIEND1</button>
+                <button onclick="window.location.href='https://steamcommunity.com'"class="btn2">ADD FRIEND2</button>
+            <hr class="hr">
+        <div class="copy">
+    <p>© The <a href="https://zullls.ru">Web</a> Developer 2021.</p>
+ </div>
+    <style>
+    /*ZULLLS.RU*/
+/* Шрифты */
+@import url('https://fonts.googleapis.com/css2?family=Assistant:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@300&display=swap');
+/* Стили */
+body {
+    background-color: rgb(255, 255, 255);
+}
+.img-center {
+    display: block;
+    position: absolute;
+    left: 800px;
+    top: 150px;
+}
+.thickfont {
+    font-family: 'Assistant:wght@300', sans-serif;
+    font-size: 25px;
+    position: absolute;
+    left: 695px;
+    top: 420px;
+}
+.font {
+    font-family: 'Assistant', sans-serif;
+    font-size: 30px;
+    position: absolute;
+    left: 580px;
+    top: 510px;
+}
+.btn1 {
+    position: absolute;
+    top: 630px;
+    left: 725px;
+	display: inline-block;	
+	box-sizing: border-box;
+	padding: 0 25px;
+	margin: 0 15px 15px 0;
+	outline: none;
+	border: 1px solid rgba(255, 255, 255, 0);
+	border-radius: 50px;
+	height: 46px;
+	line-height: 46px;
+	font-size: 14px;
+	font-weight: 600;
+	text-decoration: none;
+	color: rgb(255, 255, 255);
+	background-color: rgb(0, 0, 0);
+	box-shadow: 0 4px 6px rgb(65 132 144 / 10%), 0 1px 3px rgb(0 0 0 / 8%);
+	cursor: pointer;
+	user-select: none;
+	appearance: none;
+	touch-action: manipulation;  
+	vertical-align: top;
+	transition: box-shadow 0.2s;
+}
+.btn1:active {
+	background-color: #ffffff;
+    color: rgb(0, 0, 0); 
+}
+.btn1:disabled {
+	background-color: #eee;
+	border-color: #eee;
+	color: #444;
+	cursor: not-allowed;
+}
+.btn2 {
+    position: absolute;
+    top: 630px;
+    left: 985px;
+	display: inline-block;	
+	box-sizing: border-box;
+	padding: 0 25px;
+	margin: 0 15px 15px 0;
+	outline: none;
+	border: 1px solid rgba(255, 255, 255, 0);
+	border-radius: 50px;
+	height: 46px;
+	line-height: 46px;
+	font-size: 14px;
+	font-weight: 600;
+	text-decoration: none;
+	color: rgb(255, 255, 255);
+	background-color: rgb(0, 0, 0);
+	box-shadow: 0 4px 6px rgb(65 132 144 / 10%), 0 1px 3px rgb(0 0 0 / 8%);
+	cursor: pointer;
+	user-select: none;
+	appearance: none;
+	touch-action: manipulation;  
+	vertical-align: top;
+	transition: box-shadow 0.2s;
+}
+.btn2:active {
+	background-color: #ffffff;
+    color: rgb(0, 0, 0); 
+}
+.btn2:disabled {
+	background-color: #eee;
+	border-color: #eee;
+	color: #444;
+	cursor: not-allowed;
+}
+.hr {
+    display: block;
+    position: relative;
+    top: 1050px;
+    margin: 20px auto;
+    width: 1300px;
+    background-color: #ffffff;
+    border-top: 1px solid rgba(0, 0, 0, 0.404)
+}
+.copy { 
+    font-size: 15px;
+    position:absolute;
+    bottom: -165px;
+    left: 850px;
+    color: #000000;
+    font-family: 'Manrope';
+}
+::-webkit-scrollbar {
+    width: 3px;
+    color: rgb(255, 255, 255);
+}
+
+::-webkit-scrollbar-track {
+    background: #ffffff;
+}
+
+::-webkit-scrollbar-thumb {
+    background: rgb(0, 0, 0);
+}
+  .rectangle {
+      background-color: rgb(113, 23, 173);
+      width: 100%;
+      height: 75px;
+      box-shadow: 0 0 15px rgb(255, 255, 255);
+      padding: 10;
+  }
+    </style>
+</body>
+</html>
